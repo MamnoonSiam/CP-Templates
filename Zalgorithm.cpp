83b693d7e3d55e74f3b8863a6b68724362da70a7
Mamnoon Siam <32074720+MamnoonSiam@users.noreply.github.com>
2018-01-07 19:58:52 +0600

    Update Zalgorithm.cpp

--- Zalgorithm.cpp
+++ Zalgorithm.cpp
@@ -1,7 +1,6 @@
 vector<int> zalgo(string s) {
-	int n = s.size();
+	int n = s.size(), x = 0, y = 0;
 	vector<int> z(n);
-	int x = 0, y = 0;
 	for (int i = 1; i < n; i++) {
 		z[i] = max(0,min(z[i-x],y-i+1));
 		while (i+z[i] < n && s[z[i]] == s[i+z[i]]) {
