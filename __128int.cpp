e6a50c3e5a954075a5f37ed3fab099cead9f10b7
Mamnoon Siam <32074720+MamnoonSiam@users.noreply.github.com>
2018-01-17 02:37:54 +0600

    Create __128int.cpp

---
+++ __128int.cpp
@@ -0,0 +1,41 @@
+typedef __int128_t int128; // or __int128
+typedef __uint128_t uint128; // or unsigned __int128
+
+std::ostream& operator<<(std::ostream &outputstream, __int128_t &t) {
+	bool sign = false;
+	if(t < 0) sign = true;
+	__uint128_t num = sign ? -t : t;
+	string str = num == 0 ? "0" : "";
+	while(num) {
+		int ld = num % 10;
+	  	str += char(ld + '0');
+		num /= 10;
+	} reverse(str.begin(), str.end());
+	if(sign) str = "-" + str; outputstream << str;
+	return outputstream;
+}
+std::ostream& operator<<(std::ostream &outputstream, __uint128_t &t) {
+	__uint128_t num = t;
+	string str = num == 0 ? "0" : "";
+	while(num) {
+		int ld = num % 10;
+	  	str += char(ld + '0');
+		num /= 10;
+	} reverse(str.begin(), str.end());
+	outputstream << str;
+	return outputstream;
+}
+std::istream& operator>> (std::istream &inputstream, __int128_t &t)
+{
+	long long intputnumber; // or long long int
+	inputstream >> intputnumber;
+	t = intputnumber;
+	return inputstream;
+}
+std::istream& operator>> (std::istream &inputstream, __uint128_t &t)
+{
+	unsigned long long intputnumber; // or unsigned long long int
+	inputstream >> intputnumber;
+	t = intputnumber;
+	return inputstream;
+}
