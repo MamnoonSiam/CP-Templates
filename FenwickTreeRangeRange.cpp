26def1ef3357a8ea1492bb0ac57bf566d7f3de6b
Mamnoon Siam <32074720+MamnoonSiam@users.noreply.github.com>
2020-04-11 01:34:00 +0600

    Update FenwickTreeRangeRange.cpp

--- FenwickTreeRangeRange.cpp
+++ FenwickTreeRangeRange.cpp
@@ -1,31 +1,28 @@
 template<typename T>
 struct fenwick_tree {
   // 1-indexed, ranges are inclusive
-  T *B1, *B2;
+  vector<T> B1, B2;
   int N;
+  fenwick_tree() { N = 0; }
   fenwick_tree(int _N) : N(_N) {
-    B1 = new T[N+1], B2 = new T[N+1];
-    #define clr(v) fill(v, v+(N+1), 0)
-    clr(B1), clr(B2);
-    #undef clr
+    B1.assign(N+1, 0), B2.assign(N+1, 0);
   }
-  ~fenwick_tree() { delete [] B1, delete [] B2; }
-  void add(T* B, int idx, T value) {
-    for(; idx <= N; idx += idx & -idx) B[idx] += value;
+  void add(vector<T>& B, int p, T x) {
+    for(; p <= N; p += p & -p) B[p] += x;
   }
-  void range_add(int l, int r, T value) {
-    add(B1, l, value);
-    add(B1, r+1, -value);
-    add(B2, l, value * (l-1));
-    add(B2, r+1, -value * r);
+  void range_add(int l, int r, T x) {
+    add(B1, l, x);
+    add(B1, r+1, -x);
+    add(B2, l, x*(l-1));
+    add(B2, r+1, -x*r);
   }
-  T sum(T* B, int idx, T ret = 0) {
-    for(; idx > 0; idx -= idx & -idx) {
-      ret += B[idx];
+  T sum(vector<T>& B, int p, T ret = 0) {
+    for(; p > 0; p -= p & -p) {
+      ret += B[p];
     } return ret;
   }
-  T prefix_sum(int idx) {
-    return sum(B1, idx) * idx - sum(B2, idx);
+  T prefix_sum(int p) {
+    return sum(B1, p) * p - sum(B2, p);
   }
   T range_sum(int l, int r) {
     return prefix_sum(r) - prefix_sum(l-1);
